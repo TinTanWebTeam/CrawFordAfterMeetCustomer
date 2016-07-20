18eb3e66cdb9c19e94a123cd2016cc7d3d37699a
nvnghia <nvnghia0202@gmail.com>
2016-07-20 11:03:32 +0700

    view claim

---
+++ resources/views/admin/claim.blade.php
@@ -0,0 +1,475 @@
+<div class="row">
+    <form class="form-claim">
+        <table style="width: 100%">
+            <tr>
+                <td style="width: 15%;">
+                    <h5 class="text-right">ID:</h5>
+                </td>
+                <td style="width: 15%;">
+                    <input type="text">
+                </td>
+                <td style="width: 20%;">
+                    <div style="display: inline-block;width: 20%">
+                        <input type="checkbox">
+                    </div>
+                    <div style="display: inline-block;width: 78%">
+                        <h5>Privileged</h5>
+                    </div>
+                </td>
+                <td style="width: 25%;">
+                    <div style="display: inline-block;width: 30%">
+                        <h5 class="text-right">Account:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td style="width: 25%;">
+                    <div style="display: inline-block;width: 40%">
+                        <h5>Policy:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 30%">
+                        <h5 class="text-right">Partnership:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5>Currency:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <select name="" id="" style="width: 100%">
+                            <option value="">USD</option>
+                            <option value="">VND</option>
+                        </select>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Organization:</h5>
+                </td>
+                <td colspan="2">
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 30%">
+                        <h5 class="text-right">Incident:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5>Branch Seq #:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2">
+                    <div style="display: inline-block;width: 30%">
+
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <h5 class="text-left">First Name:</h5>
+                    </div>
+                </td>
+                <td>
+                    <h5 class="text-left">Last Name:</h5>
+                </td>
+                <td>
+                    <h5 class="text-center" style="border-bottom: 1px solid #7d7676">Dates</h5>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Insured Name:</h5>
+                </td>
+                <td colspan="2">
+                    <div style="display: inline-block;width: 30%">
+                        <button style="width: 100%">Address...</button>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Received:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Insured Claim #:</h5>
+                </td>
+                <td colspan="3">
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Opened:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Operated As:</h5>
+                </td>
+                <td colspan="3">
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Closed:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Misc Info:</h5>
+                </td>
+                <td colspan="3">
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Reopened:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Claim Type:</h5>
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 20%">
+                        <h5>Insurer:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 78%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 30%">
+                        <h5 class="text-right">Branch:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Destroy:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Loss Desc:</h5>
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 20%">
+                        <h5>Broker:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 78%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 30%">
+                        <h5 class="text-right">Adjuster:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">EBox Destroy:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 30%">
+                        <h5 class="text-right">Rate:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">First Contact:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Catastrophic Loss:</h5>
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 20%">
+                        <h5>Source:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 78%">
+                        <input type="text">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 30%">
+                        <h5 class="text-right">Period:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 68%">
+                        <select name="" id="" style="width: 100%">
+                            <option value="">Hourly</option>
+                            <option value="">Flat</option>
+                        </select>
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Contact:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <select name="" id="" style="width: 100%">
+                            <option value="">Nguyen</option>
+                        </select>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">
+                    <button>Loss Location</button>
+                </td>
+                <td colspan="3">
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Loss Date:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Line Of Business Code:</h5>
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 50%">
+                        <h5 class="text-right">Large Loss Claim:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 48%">
+                        <select name="" id="">
+                            <option value="">Undetermined</option>
+                            <option value="">Determined</option>
+                        </select>
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 68%">
+                        <h5 class="text-right">Taxable:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 30%">
+                        <input type="checkbox">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Proscription:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5 class="text-right">Estimated Claim Value:</h5>
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 50%">
+                        <h5 class="text-right">SIR Breached:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 48%">
+                        <input type="checkbox">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 68%">
+                        <h5 class="text-right">Claim Assignment:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 30%">
+                        <input type="checkbox">
+                    </div>
+                </td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Policy Inception:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4"></td>
+                <td>
+                    <div style="display: inline-block;width: 40%">
+                        <h5 class="text-right">Policy Expiry:</h5>
+                    </div>
+                    <div style="display: inline-block;width: 58%">
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <h5></h5>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" rowspan="6">
+                    <h5>Co-insurers Detail</h5>
+                    <table>
+                        <tr>
+                            <th>
+                                Bill To
+                            </th>
+                            <th>
+                                Lead
+                            </th>
+                        </tr>
+                        <tr>
+
+                        </tr>
+                    </table>
+                </td>
+                <td rowspan="6" style="border: 1px solid #c5c0c0;border-radius: 10px;padding: 10px">
+                    <div>
+                        <div style="display: inline-block;width: 40%">
+                            <h5 class="text-right">Created Date:</h5>
+                        </div>
+                        <div style="display: inline-block;width: 58%">
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div>
+                        <div style="display: inline-block;width: 40%">
+                            <h5 class="text-right">Created By:</h5>
+                        </div>
+                        <div style="display: inline-block;width: 58%">
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div>
+                        <div style="display: inline-block;width: 40%">
+                            <h5 class="text-right">Last Changed:</h5>
+                        </div>
+                        <div style="display: inline-block;width: 58%">
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div>
+                        <div style="display: inline-block;width: 40%">
+                            <h5 class="text-right">Last Changed By:</h5>
+                        </div>
+                        <div style="display: inline-block;width: 58%">
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div>
+                        <div style="display: inline-block;width: 40%">
+                            <h5 class="text-right">Import Date:</h5>
+                        </div>
+                        <div style="display: inline-block;width: 58%">
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div>
+                        <div style="display: inline-block;width: 40%">
+                            <h5 class="text-right">Import Close Date:</h5>
+                        </div>
+                        <div style="display: inline-block;width: 58%">
+                            <input type="text">
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <br>
+    <br>
+</div>
