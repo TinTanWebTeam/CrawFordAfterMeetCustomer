12a2a41c129548427c5a0d33e3cd9ff0bb11f902
Nguyen Phuoc Hoa <nphoa7777@gmail.com>
2016-07-20 15:58:28 +0700

    nphoa_commit view invoice

--- resources/views/admin/invoice.blade.php
+++ resources/views/admin/invoice.blade.php
@@ -1,3 +1,392 @@
-<div class="row">
-    
-</div>
+<form action="" class="form-invoice">
+    <div class="row">
+        <div class="col-sm-7">
+            <div class="row" style="padding: 10px;border: 1px solid #AFAFAF">
+                <fieldset>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <div class="col-sm-5">
+                                    <h5 style="text-align: right">Invoice #</h5>
+                                </div>
+                                <div class="col-sm-7">
+                                    <input type="text" name="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <h5 style="text-align: left">Invoice Date</h5>
+                                </div>
+                                <div class="col-sm-8">
+                                    <input type="date" name="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+            </div>
+            <div class="row" style="padding: 10px;border: 1px solid #AFAFAF;margin-top: 10px">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Bill To:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Billing Type:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3px">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3px">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3px">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3px">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3px">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3px">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding: 10px;border: 1px solid #AFAFAF;margin-top: 10px">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="text-align: right">Organization:</h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Claim #:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Loss Desc:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">AdjusterID:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">BranchID:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="text-align: right">Insured Name:</h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="text-align: right">Policy #:</h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="text-align: right">Co. Claim #:</h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="text-align: right">Node:</h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="" name="">
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding: 10px;border: 1px solid #AFAFAF;margin-top: 10px">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Master Billed:</h5>
+                            </div>
+                            <div class="col-sm-2">
+                                <input type="checkbox" name="">
+                            </div>
+                            <div class="col-sm-7">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Master Bill #:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Print Date:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Printed By:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">UserID:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">BatchID:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">Invoice Credit Code:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h5 style="text-align: right">ID:</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <input type="text" name="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-5">
+            <div class="row" style="padding: 10px;border: 1px solid #AFAFAF;margin-left: 10px">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Professional</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">General Exp</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Comm & Photo Exp</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Consult Fees & Exp</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Travel Related Exp</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">GST-free Disb</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Disbursements</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Claim Total Tax</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Claim Total Fee</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Your Subtotal</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Your Taxes</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="text-align: right">Your Position</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" name="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</form>
