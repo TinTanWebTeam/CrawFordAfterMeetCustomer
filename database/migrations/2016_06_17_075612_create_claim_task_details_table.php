039ffb61790fcc70cfd604d17eb07d3248d5332c
nvnghia <nvnghia0202@gmail.com>
2016-08-01 16:29:24 +0700

    afsfsa

--- database/migrations/2016_06_17_075612_create_claim_task_details_table.php
+++ database/migrations/2016_06_17_075612_create_claim_task_details_table.php
@@ -15,18 +15,35 @@ class CreateClaimTaskDetailsTable extends Migration
         Schema::create('claim_task_details', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('professionalServices')->nullable();//cong viec chinh if not equal 0 will be reference to id in task_category
+            //unit time
             $table->decimal('professionalServicesTime')->nullable();
+            $table->decimal('professionalServicesTimeBillValue')->nullable();
+            $table->decimal('professionalServicesTimeOverrideValue')->nullable();
+            
             $table->string('professionalServicesNote')->nullable();
+            //rate
+            $table->decimal('professionalServicesRate')->nullable();
+            $table->decimal('professionalServicesRateBillValue')->nullable();
+            $table->decimal('professionalServicesRateOverrideValue')->nullable();
+            //amount
+            $table->decimal('professionalServicesAmount')->nullable();
+            $table->decimal('professionalServicesAmountBillValue')->nullable();
+            $table->decimal('professionalServicesAmountOverrideValue')->nullable();
+
             $table->integer('expense')->nullable();// cong viec phu
-            $table->decimal('expenseTime')->nullable();
+
+            $table->decimal('expenseAmount')->nullable();
+            $table->decimal('expenseAmountBillValue')->nullable();
+            $table->decimal('expenseAmountOverrideValue')->nullable();
+            
             $table->string('expenseNote')->nullable();
+
             $table->boolean('active')->default(1);
             $table->integer('claimId')->default(0);//if not equal 0 will be reference to id in Claim
             $table->integer('statusId')->default(0); //if not equal 0 will be reference to id in status
             $table->integer('userId')->default(0); //if not equal 0 will be reference to id in user
             $table->integer('createdBy')->default(0); //if not equal 0 will be reference to id in user
             $table->integer('updatedBy')->default(0); //if not equal 0 will be reference to id in user
-
 
             $table->timestamps();
         });
