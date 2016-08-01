15a7e6fc8a5fcc52bd5ba3aeaf724979c93df447
Nguyen Phuoc Hoa <nphoa7777@gmail.com>
2016-08-01 09:51:24 +0700

    nphoa_commit

--- resources/views/partials/user/header.blade.php
+++ resources/views/partials/user/header.blade.php
@@ -14,8 +14,8 @@
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Link</a></li>
-				<li><a href="#">Link</a></li>
+				<li><a href="javascript:;">Information</a></li>
+				<li><a href="javascript:;">Task</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
 				<li class="dropdown">
