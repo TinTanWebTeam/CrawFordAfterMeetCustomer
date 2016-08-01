9e93502805eda2b28bac2a08e64ffcfce80be563
nvnghia <nvnghia0202@gmail.com>
2016-08-01 10:56:38 +0700

    asfsaf

--- resources/views/admin/docket.blade.php
+++ resources/views/admin/docket.blade.php
@@ -1,17 +1,65 @@
 <div class="row">
     <form action="" style="background-color: #fff;width: 100%">
+        <div class="row" style="padding: 20px;">
+            <div class="col-sm-4">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="display: inline-block" class="text-right">Claim Id:</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" id="claimId" value="" style="display: inline-block">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 style="display: inline-block" class="text-right">Employee Id:</h5>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" id="employeeId" value="" style="display: inline-block">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="display: inline-block" class="text-right">Insured Name: </h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" id="insuredName" value="" style="display: inline-block;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="display: inline-block" class="text-right">Loss Date: </h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" id="lossDate" value="" style="display: inline-block;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h5 style="display: inline-block" class="text-right">Loss Location: </h5>
+                    </div>
+                    <div class="col-sm-10">
+                        <input type="text" id="lossLocation" value="" style="display: inline-block;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <form action="" style="background-color: #fff;width: 100%">
         <div class="row">
             <div class="col-sm-6" >
-                <div class="row" >
+                <div class="row" style="padding: 20px;">
                     <div class="col-sm-12" >
-                        <fieldset style="border: 1px solid black">
+                        <fieldset>
                             <legend>Time:</legend>
                             <div class="row">
                                 <div class="col-sm-3">
                                     <h5 style="text-align:right">Code:</h5>
                                 </div>
                                 <div class="col-sm-9">
-                                    <input type="" name="" value="1">
+                                    <input type="" name="" value="1" style="width: auto">
                                 </div>
                             </div>
                             <div class="row">
@@ -26,7 +74,7 @@
                         </fieldset>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row" style="padding: 20px;">
                     <div class="col-sm-12">
                         <fieldset>
                             <legend>Expense:</legend>
@@ -35,7 +83,7 @@
                                     <h5 style="text-align:right">Code:</h5>
                                 </div>
                                 <div class="col-sm-9">
-                                    <input type="" name="" value="1">
+                                    <input type="" name="" value="1"  style="width: auto">
                                 </div>
                             </div>
                             <div class="row">
@@ -53,7 +101,7 @@
 
             </div>
             <div class="col-sm-6">
-                <div class="row">
+                <div class="row" style="padding: 20px;">
                     <div class="col-sm-12">
                         <fieldset>
                             <legend>Details:</legend>
@@ -117,7 +165,7 @@
                         </fieldset>
                     </div>
                 </div>
-                <div class="row" style="margin-top:14px">
+                <div class="row" style="margin-top:14px;padding: 20px;">
                     <div class="col-sm-12">
                         <fieldset>
                             <legend>Details:</legend>
@@ -215,91 +263,98 @@
                 </div>
             </div>
         </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12" style="padding-right: 40px">
+                <button class="btn btn-sm btn-default pull-right">Cancel</button>
+                <button class="btn btn-sm btn-default pull-right">Save</button>
+            </div>
+        </div>
+        <br>
     </form>
     <table style="background-color: #fff;width: 100%" class="table-claim-report-assist">
         <thead>
         <tr class="assist">
-            <th colspan="9"><h4 class="text-center">Assists</h4></th>
+            <th colspan="10"><h4 class="text-center">Docket</h4></th>
         </tr>
         <tr style="border-bottom: 1px solid black;">
-            <th class="text-bold">Branch</th>
-            <th class="text-bold">Adjuster First Name</th>
-            <th class="text-bold">Adjuster Last Name</th>
-            <th class="text-bold">Rate</th>
-            <th class="text-bold">Fee Type</th>
-            <th class="text-bold">Diarize</th>
-            <th class="text-bold">Dairy Date</th>
-            <th class="text-bold">Assist Completed</th>
-            <th class="text-bold">Date Added</th>
+            <th class="text-bold text-center">Date</th>
+            <th class="text-bold text-center">Adjuster Code</th>
+            <th class="text-bold text-center">Professional Code</th>
+            <th class="text-bold text-center">Professional Unit</th>
+            <th class="text-bold text-center">Professional Note</th>
+            <th class="text-bold text-center">Expense Code</th>
+            <th class="text-bold text-center">Expense Unit</th>
+            <th class="text-bold text-center">Expense Note</th>
+            <th class="text-bold text-center">Invoice MajorNo</th>
+            <th class="text-bold text-center">Invoice Date</th>
         </tr>
         </thead>
         <tbody>
         <tr>
-            <td>HC1A</td>
-            <td>Thi Minh Hanh</td>
-            <td>Luu</td>
-            <td>90.00</td>
-            <td>Hourly</td>
-            <td>No</td>
-            <td></td>
-            <td>Yes</td>
-            <td>09-Dec-2015</td>
+            <td class="text-center">03-Sep-2015</td>
+            <td class="text-center">SONV1</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">2703</td>
+            <td class="text-center">09-Dec-2015</td>
         </tr>
         <tr>
-            <td>HC1A</td>
-            <td>Thi Minh Hanh</td>
-            <td>Luu</td>
-            <td>90.00</td>
-            <td>Hourly</td>
-            <td>No</td>
-            <td></td>
-            <td>Yes</td>
-            <td>09-Dec-2015</td>
+            <td class="text-center">03-Sep-2015</td>
+            <td class="text-center">SONV1</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">2703</td>
+            <td class="text-center">09-Dec-2015</td>
         </tr>
         <tr>
-            <td>HC1A</td>
-            <td>Thi Minh Hanh</td>
-            <td>Luu</td>
-            <td>90.00</td>
-            <td>Hourly</td>
-            <td>No</td>
-            <td></td>
-            <td>Yes</td>
-            <td>09-Dec-2015</td>
+            <td class="text-center">03-Sep-2015</td>
+            <td class="text-center">SONV1</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">2703</td>
+            <td class="text-center">09-Dec-2015</td>
         </tr>
         <tr>
-            <td>HC1A</td>
-            <td>Thi Minh Hanh</td>
-            <td>Luu</td>
-            <td>90.00</td>
-            <td>Hourly</td>
-            <td>No</td>
-            <td></td>
-            <td>Yes</td>
-            <td>09-Dec-2015</td>
+            <td class="text-center">03-Sep-2015</td>
+            <td class="text-center">SONV1</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">2703</td>
+            <td class="text-center">09-Dec-2015</td>
         </tr>
         <tr>
-            <td>HC1A</td>
-            <td>Thi Minh Hanh</td>
-            <td>Luu</td>
-            <td>90.00</td>
-            <td>Hourly</td>
-            <td>No</td>
-            <td></td>
-            <td>Yes</td>
-            <td>09-Dec-2015</td>
-        </tr>
-        <tr>
-            <td>HC1A</td>
-            <td>Thi Minh Hanh</td>
-            <td>Luu</td>
-            <td>90.00</td>
-            <td>Hourly</td>
-            <td>No</td>
-            <td></td>
-            <td>Yes</td>
-            <td>09-Dec-2015</td>
+            <td class="text-center">03-Sep-2015</td>
+            <td class="text-center">SONV1</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">SV</td>
+            <td class="text-center">3.5</td>
+            <td>Site Visit: ...</td>
+            <td class="text-center">2703</td>
+            <td class="text-center">09-Dec-2015</td>
         </tr>
         </tbody>
     </table>
+    <br>
+    <br>
+    <br>
+    <br>
 </div>
