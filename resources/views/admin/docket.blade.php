bbae99bb96e541e7db46d8e689dfa977bde8b378
Nguyen Phuoc Hoa <nphoa7777@gmail.com>
2016-08-01 09:56:25 +0700

    commit task

--- resources/views/admin/docket.blade.php
+++ resources/views/admin/docket.blade.php
@@ -1,6 +1,220 @@
 <div class="row">
     <form action="" style="background-color: #fff;width: 100%">
+        <div class="row">
+            <div class="col-sm-6" >
+                <div class="row" >
+                    <div class="col-sm-12" >
+                        <fieldset style="border: 1px solid black">
+                            <legend>Time:</legend>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h5 style="text-align:right">Code:</h5>
+                                </div>
+                                <div class="col-sm-9">
+                                    <input type="" name="" value="1">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h5 style="text-align:right">Sort Description:</h5>
+                                </div>
+                                <div class="col-sm-9">
+			    			<textarea rows="4" cols="40" style="resize: none;">
+			    			</textarea>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <fieldset>
+                            <legend>Expense:</legend>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h5 style="text-align:right">Code:</h5>
+                                </div>
+                                <div class="col-sm-9">
+                                    <input type="" name="" value="1">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h5 style="text-align:right">Sort Description:</h5>
+                                </div>
+                                <div class="col-sm-9">
+		    			<textarea rows="4" cols="40" style="resize: none;">
+		    			</textarea>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+                </div>
 
+            </div>
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <fieldset>
+                            <legend>Details:</legend>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <fieldset>
+                                        <legend style="font-size:15px">Initial Values:</legend>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Units</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Amount</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                                <div class="col-sm-4">
+                                    <fieldset>
+                                        <legend style="font-size:15px">Billable values:</legend>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Units</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                                <div class="col-sm-4">
+                                    <fieldset>
+                                        <legend style="font-size:15px">User Overide:</legend>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Units</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Amount</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+                </div>
+                <div class="row" style="margin-top:14px">
+                    <div class="col-sm-12">
+                        <fieldset>
+                            <legend>Details:</legend>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <fieldset>
+                                        <legend style="font-size:15px">Initial Values:</legend>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Amount</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Units</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Rate/Unit</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                                <div class="col-sm-4">
+                                    <fieldset>
+                                        <legend style="font-size:15px">Billable values:</legend>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Amount</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Units</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Rate/Unit</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                                <div class="col-sm-4">
+                                    <fieldset>
+                                        <legend style="font-size:15px">User Overide:</legend>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Amount</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Unit</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h5 style="text-align:right">Rate/Unit</h5>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <input type="" name="" value="1" style="width:80px">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
+        </div>
     </form>
     <table style="background-color: #fff;width: 100%" class="table-claim-report-assist">
         <thead>
